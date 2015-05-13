d3730637a74975936767812abae5e81b8eac12ae
aszmyd <adam.szmyd@polcode.net>
2015-05-13 14:35:29 +0200

    Change karma coverage sources

--- karma.conf.js
+++ karma.conf.js
@@ -36,10 +36,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/animations/*js': 'coverage',
-      'src/animate.js': 'coverage',
-      'src/animationsAssist.js': 'coverage',
-      'src/animationClass.js': 'coverage'
+      'src/**/*js': 'coverage'
     },
 
 
